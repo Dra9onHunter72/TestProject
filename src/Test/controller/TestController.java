92fd32bd28feafd822900e4baa60d8fbecabe6c2
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-09-07 12:45:53 -0600

    I added some code

---
+++ src/Test/controller/TestController.java
@@ -0,0 +1,11 @@
+package Test.controller;
+
+public class TestController
+{
+
+	public void start()
+	{
+		System.out.println("This is a test project");
+		System.out.println("I can do it :D");
+	}
+}
---
+++ src/Test/controller/TestRunner.java
@@ -0,0 +1,10 @@
+package Test.controller;
+
+public class TestRunner
+{
+	public static void main(String [] args)
+	{
+	TestController appController = new TestController();
+	appController.start();
+	}
+}
